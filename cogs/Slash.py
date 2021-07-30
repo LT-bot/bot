634f4d8cecb984209b9126e2a8f0b24d0978321f
foo <krowlaxs@gmail.com>
2021-07-30 21:09:58 +0100

    webhook fix

--- cogs/Slash.py
+++ cogs/Slash.py
@@ -14,25 +14,41 @@ default_avatar = conf['Slash']['default avatar']
 secret_num = int(conf['Slash']['secret num'])
 remove_fslash = str.maketrans("/", " ")
 
-async def as_webhook(channel: discord.TextChannel, avatar_url: str, name: str, 
-        message: str, attachments: Optional[List[discord.Attachment]]=None ) -> None:
-    """
-    Send a message through a webhook. May contain list of files. No mentions
-    because webhooks can't be blocked and it's annoying.
-    """
-    opts = {'content': message,
-            'files': attachments,
-            'avatar_url': avatar_url,
-            'username': name,
-            'allowed_mentions': discord.AllowedMentions.none()
-            }
-    webhook = await channel.webhooks()
-    await webhook[0].send(**opts)
 
 class Slash(commands.Cog):
     def __init__(self, Bot):
         self.Bot = Bot
-        self.cog_before_invoke = self.find_webhooks
+        self.webhooks = {}
+        #self.cog_before_invoke = self.find_webhooks
+
+    async def as_webhook(self, channel: discord.TextChannel, avatar_url: str, name: str, 
+            message: str, attachments: Optional[List[discord.Attachment]]=None ) -> None:
+        """
+        Send a message through a webhook. May contain list of files. No mentions
+        because webhooks can't be blocked and it's annoying.
+        """
+        opts = {'content': message,
+                'files': attachments,
+                'avatar_url': avatar_url,
+                'username': name,
+                'allowed_mentions': discord.AllowedMentions.none()
+                }
+        #webhook = await channel.webhooks()
+        try:
+            webhook = self.webhooks[channel.id]
+        except KeyError:
+            pass
+        await webhook[0].send(**opts)
+    
+    @commands.command(name='init', aliases=['i'])
+    async def _init(self, context: commands.Context):
+        chan = context.channel
+        hooks = await chan.webhooks()
+        for hook in hooks:
+            if hook.name == 'lt_bot_hooker':
+                self.webhooks[chan.id] = hook
+                return
+        self.webhooks[chan.id] = await chan.create_webhook('lt_bot_hooker')
 
     async def find_webhooks(context):
         print('aaaaa')
@@ -62,7 +78,7 @@ class Slash(commands.Cog):
         
         name = context.author.nick or context.author.name
 
-        await as_webhook(context.channel, context.author.avatar_url, 
+        await self.as_webhook(context.channel, context.author.avatar_url, 
                 name, message)
         await context.send('ok', delete_after=0.01)
 
@@ -85,7 +101,7 @@ class Slash(commands.Cog):
         #this isn't cryptographically safe or anything, but it's faster
         id = (context.author.id + round(time(),-3))%secret_num
 
-        await as_webhook(context.channel, default_avatar,
+        await self.as_webhook(context.channel, default_avatar,
                 f"Anon {id}", message.translate(remove_fslash))
         await context.send('ok', hidden=True)
 
